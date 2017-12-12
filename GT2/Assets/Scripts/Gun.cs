f5025bb321b473fd974762eaaed131072e002519
Lenny Gingello <lcg397@gmail.com>
2017-12-12 17:44:52 -0500

    Zxcscscscs

---
+++ GT2/Assets/Scripts/Gun.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+
+
+    public GameObject FirePoint, Bullet;
+
+    // Use this for initialization
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FireBullet();
+        }
+    }
+    
+
+
+
+
+    public void FireBullet()
+    {
+
+        Instantiate(Bullet, FirePoint.transform.position, FirePoint.transform.rotation);
+
+
+    }
+}
---
+++ Gingello_Final/Gingello_Final_Unity_2017.2.0f3/Assets/Scripts/RubbleBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class RubbleBehavior : MonoBehaviour {
+    float TimeD;
+    public ParticleSystem Rubb;
+    public Mesh PartMesh;
+    public GameObject Daddy;
+    private void Awake()
+    {
+        Rubb = this.gameObject.GetComponent<ParticleSystem>();
+        Daddy = this.gameObject.transform.parent.gameObject;
+        PartMesh = Daddy.GetComponent<MeshFilter>().sharedMesh;
+        var sh = Rubb.shape;
+        sh.enabled = true;
+        sh.shapeType = ParticleSystemShapeType.Mesh;
+        sh.mesh = PartMesh;
+        TimeD = 2f;
+    }
+  
+
+
+
+
+
+
+}
